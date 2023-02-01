7acbfb1e9ae9643b2b574622ad271ddec079d868
Angela Jiang <angelajiang@stripe.com>
2023-02-01 14:51:24 -0800

    removing appfee

--- pages/api/stripe/checkout.tsx
+++ pages/api/stripe/checkout.tsx
@@ -5,6 +5,7 @@ const stripe = require('stripe')(process.env.STRIPE_SK);
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     try {
         const body = JSON.parse(req.body)
+        const appFee = body.price * 0.05 //take an application fee
         
         const session = await stripe.checkout.sessions.create({
             success_url: `${process.env.NEXT_PUBLIC_HOST}/shop?success=true`,
@@ -24,7 +25,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
             ],
             mode: 'payment',
             payment_intent_data: {
-              application_fee_amount: body.price*0.05,
+              application_fee_amount: appFee,
             },
           }, {stripeAccount: body.account_id});
         
